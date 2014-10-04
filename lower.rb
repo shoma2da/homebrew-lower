1446b2785d74526fb2a8c5a058e008f991303875
shoma2da <marcos.shoichi.matsuda@gmail.com>
2014-10-04 17:34:19 +0900

    homebrew用のファイルを作成

---
+++ lower.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Lower < Formula
+  homepage "https://github.com/shoma2da/homebrew-lower"
+  url "https://github.com/shoma2da/homebrew-lower.git", :tag => "1.0.0"
+  sha1 "bb0de35104e9f0a3f2afc9713fda10f496422a51"
+  version "1.0.0"
+
+  skip_clean 'bin'
+
+  def install
+    prefix.install 'bin'
+    bin.chmod 0755
+  end
+end
